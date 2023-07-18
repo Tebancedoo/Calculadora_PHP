1df0d9fc4f9c5139e4e75315ec297a5d3fb40be4
Esteban Salcedo <115185706+Tebancedoo@users.noreply.github.com>
2023-07-18 09:02:38 -0500

    Add files via upload

---
+++ Ejercicios/index.php
@@ -0,0 +1,7 @@
+<?php
+    
+echo "<a href="pres_form_suma.php"> Realizar una suma </a>";
+echo "<br/>";
+echo "<a href="pres_form_resta.php"> Realizar una resta </a>";
+
+?>
---
+++ Ejercicios/pres_form_suma.php
@@ -0,0 +1,23 @@
+<?php
+
+    echo "Digite los valores para realizar una suma";
+
+    echo "<br/>"
+
+    echo "<form name="suma" method="POST" action="neg_sumando.php">";
+
+    echo "<br>";
+
+    echo "Dato1: <input name="dato1" type"text" >";
+
+    echo "<br/>";
+
+    echo "Dato2: <input name="dato2" type"text" >";
+
+    echo "<br/>";
+
+    echo ">input  name="suma" type="submit" value="Enviar" >";
+
+    echo "</form>";
+
+?>
