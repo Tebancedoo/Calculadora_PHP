cd2368f6241977176990955afabfececb21cfd1b
Esteban Salcedo <115185706+Tebancedoo@users.noreply.github.com>
2023-08-02 10:51:06 -0500

    Add files via upload

--- Registrar mascota/bdmascotas.sql
+++ Registrar mascota/bdmascotas.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 01-08-2023 a las 21:37:35
--- Versión del servidor: 10.1.38-MariaDB
--- Versión de PHP: 7.3.2
+-- Tiempo de generación: 02-08-2023 a las 17:50:10
+-- Versión del servidor: 10.4.27-MariaDB
+-- Versión de PHP: 8.1.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -29,21 +28,11 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `mascotas` (
-  `nombres_dueño` text CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
-  `apellidos_dueño` text CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
   `documento_dueño` varchar(30) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
-  `numero_celular` varchar(30) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
   `nombre_mascota` text CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
   `tipo_mascota` varchar(30) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
   `documento_mascota` varchar(30) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Volcado de datos para la tabla `mascotas`
---
-
-INSERT INTO `mascotas` (`nombres_dueño`, `apellidos_dueño`, `documento_dueño`, `numero_celular`, `nombre_mascota`, `tipo_mascota`, `documento_mascota`) VALUES
-('Draco', 'Perez Gomez', '000', '456123', 'Triton', '1', '555');
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- --------------------------------------------------------
 
@@ -54,7 +43,7 @@ INSERT INTO `mascotas` (`nombres_dueño`, `apellidos_dueño`, `documento_dueño`
 CREATE TABLE `tipo_mascotas` (
   `id_mascota` int(30) NOT NULL,
   `mascota` varchar(30) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Volcado de datos para la tabla `tipo_mascotas`
@@ -67,6 +56,19 @@ INSERT INTO `tipo_mascotas` (`id_mascota`, `mascota`) VALUES
 (4, 'tortuga'),
 (5, 'conejo'),
 (6, 'raton');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `nombres_dueño` text CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  `apellidos_dueño` text CHARACTER SET utf32 COLLATE utf32_spanish_ci NOT NULL,
+  `documento_dueño` varchar(30) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  `numero_celular` varchar(30) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Índices para tablas volcadas
