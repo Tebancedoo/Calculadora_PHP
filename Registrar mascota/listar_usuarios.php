7b6ff8526f3fc3978c3bea09b895aff1fe9c596c
Esteban Salcedo <115185706+Tebancedoo@users.noreply.github.com>
2023-08-08 08:59:47 -0500

    Add files via upload

--- Registrar mascota/listar_usuarios.php
+++ Registrar mascota/listar_usuarios.php
@@ -2,8 +2,7 @@
 
 include ('conexion.php');
 
-
-$sql = "SELECT * FROM usuarios";
+$sql = "SELECT * FROM mascotas"; //Inicia la primera consulta
 if (!$result = $db->query($sql))
 {
     die ('Hay un error en la consulta o los datos no se han podido encontrar [' . $db->error .  ']');
@@ -11,24 +10,36 @@ if (!$result = $db->query($sql))
 
 while ($row = $result->fetch_assoc())
 {
-    $nnombres_dueño=stripslashes($row["nombres_dueño"]);
-    $aapellidos_dueño=stripslashes($row["apellidos_dueño"]);
-    
-    echo $nnombres_dueño, " ", $aapellidos_dueño; echo "<br>";
-}//fin de la consulta
+    $nnombre_mascota=stripslashes($row["nombre_mascota"]); //Traigo el nombre de la mascota
+    $ddocumento_dueño=stripslashes($row["documento_dueño"]); // Traigo el documento del dueño de la mascota
 
+/////////////////////////////////////////// Inicia la sub consulta
 
-$sql = "SELECT * FROM mascotas";
-if (!$result = $db->query($sql))
+$sql2 = "SELECT * FROM usuarios WHERE documento_dueño='$ddocumento_dueño'";//Uso el documento del dueño para buscar el dueño
+if (!$result2 = $db->query($sql2))
 {
     die ('Hay un error en la consulta o los datos no se han podido encontrar [' . $db->error .  ']');
 }
 
-while ($row = $result->fetch_assoc())
+while ($row2 = $result2->fetch_assoc())
 {
-    $nnombre_mascota=stripslashes($row["nombre_mascota"]);
+    $nnombres_dueño=stripslashes($row2["nombres_dueño"]);
+    $aapellidos_dueño=stripslashes($row2["apellidos_dueño"]);
     
-    echo "El nombre de la mascota es: ", $nnombre_mascota; echo "<br>";
+    echo  $nnombre_mascota; echo "--"; echo $nnombres_dueño, " ", $aapellidos_dueño; echo "<br>";
+
 }//fin de la consulta
+
+///////////////////////////////////// Fin de la sub consulta
+
+}//fin de la consulta
+
+
+
+
+
+
+
+
 
 ?>
