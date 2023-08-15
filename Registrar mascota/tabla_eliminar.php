540b184de7cce13eff1ec08d61cc962573525731
Esteban Salcedo <115185706+Tebancedoo@users.noreply.github.com>
2023-08-15 07:43:13 -0500

    Add files via upload

--- Registrar mascota/tabla_eliminar.php
+++ Registrar mascota/tabla_eliminar.php
@@ -26,7 +26,10 @@ class usuario
         echo "<thead>";
         echo "<tr>";
 
+        echo "<th> Nombres dueño </th>";
+        echo "<th> Apellidos dueño </th>";
         echo "<th> Documento dueño </th>";
+        echo "<th> Nombre mascota </th>";
         echo "<th> Documento mascota </th>";
         echo "<th> Eliminar registro </th>";
 
@@ -40,21 +43,48 @@ class usuario
         {
             $ddocumento_dueño=stripslashes($row["documento_dueño"]); // Traigo el documento del dueño de la mascota
             $ddocumento_mascota=stripslashes($row["documento_mascota"]); //Traigo el documento de la mascota
-                
-                echo "<tr>
 
-                    <td> $ddocumento_dueño </td> 
-                    <td> $ddocumento_mascota </td>
+                $sql2 = "SELECT * FROM usuarios WHERE documento_dueño ='$ddocumento_dueño'"; //Segunda consulta a la tabla mascotas
+                if (!$result2 = $db->query($sql2))
+                {
+                    die ('Hay un error en la consulta o los datos no se han podido encontrar [' . $db->error .  ']');
+                }
 
-                    <td> <form method='post' action='neg_eliminar_registro.php'> 
-                    <input type='text' value='$ddocumento_dueño' hidden name='documento_dueño'> 
-                    <input type='text' value='$ddocumento_mascota' hidden name='documento_mascota'> 
-                    <input type='submit' value='Eliminar'>
-                    </form>
-                    </td>
+                while ($row2 = $result2->fetch_assoc())
+                {
+                    $nnombres_dueño=stripslashes($row2["nombres_dueño"]); // Traigo los nombres del dueño de la mascota
+                    $aapellidos_dueño=stripslashes($row2["apellidos_dueño"]); // Traigo los apellidos del dueño de la mascota
+                    
 
-                    </tr>";
+                        $sql3 = "SELECT * FROM mascotas WHERE documento_mascota ='$ddocumento_mascota'"; //Tercera consulta a la tabla mascotas
+                        if (!$result3 = $db->query($sql3))
+                        {
+                            die ('Hay un error en la consulta o los datos no se han podido encontrar [' . $db->error .  ']');
+                        }
 
+                        while ($row3 = $result3->fetch_assoc())
+                        {
+                            $nnombre_mascota=stripslashes($row3["nombre_mascota"]); // Traigo los nombres de la mascota 
+
+                            echo "<tr>
+
+                                <td> $nnombres_dueño </td>
+                                <td> $aapellidos_dueño </td>
+                                <td> $ddocumento_dueño </td> 
+                                <td> $nnombre_mascota </td>
+                                <td> $ddocumento_mascota </td>
+                                
+
+                                <td> <form method='post' action='neg_eliminar_registro.php'> 
+                                <input type='text' value='$ddocumento_dueño' hidden name='documento_dueño'> 
+                                <input type='text' value='$ddocumento_mascota' hidden name='documento_mascota'> 
+                                <input type='submit' value='Eliminar'>
+                                </form>
+                                </td>
+
+                            </tr>";
+                        }
+                }
         }//fin de la consulta 
 
         //hacer un boton para eliminar los registros, crear una capa para eliminar los registros, separar la consulta (crar una nueva consulta para hacer el eliminar y no en el ultimo ya existente)
